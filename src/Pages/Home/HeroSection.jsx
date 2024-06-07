66b292c543569ff72dbf04bf4cc3daf602258b40
pikca-c <vincimchan@gmail.com>
2024-06-07 18:00:17 +0100

    Finished herosection css and updated errors

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
 return(
     <section id="heroSection" className="hero--section">
-        <div className="hero--section--content--box">
+        <div className="hero--section--content-box">
             <div className="hero--section--content">
                 <p className="section--title">Welcome</p>
                 <h1 className="hero--section--title">
@@ -17,7 +17,7 @@ return(
                     Reiciendis totam nostrum adipisci corrupti? Quae!
                 </p>
             </div>
-            <button className="btn btn--primary">Get in Touch</button>
+            <button className="btn btn-primary">Get in Touch</button>
         </div>
         <div className="hero--section--img">
         <img src="./img/hero_img.png" alt="HeroSection"/>
