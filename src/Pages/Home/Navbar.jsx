8ecc65496f91f7df8a37d234ce0ca3a199e36132
pikca-c <vincimchan@gmail.com>
2024-06-07 23:11:27 +0100

    navbar update

---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-scroll";
+
+function Navbar() {
+    const { navActive, setNavActive } = useState(false);
+
+    const toggleNav = () => {
+        setNavActive(!navActive);
+}
+
+const closeMenu = () => {
+    setNavActive(false);
+}
+
+useEffect(() => {
+    const handleResize = () => {
+        if (window.innerWidth <= 500) {
+            closeMenu();
+        }
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+        window.removeEventListener("resize", handleResize);
+    };
+}, []);
+
+useEffect(() => {
+    if(window.innerwidth <= 1200) {
+        closeMenu();
+    }
+}, []);
+
+return (
+    <nav className = {`navbar ${navActive? "active": ""}`}>
+        <div>
+            <img src="./img/logo.png" alt="Logoipsum" />
+        </div>
+    </nav>
+);
+}
+
+export default Navbar;
