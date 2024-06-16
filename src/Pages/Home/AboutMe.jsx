ceb119c1b7063e21b3dba9cf2d2a4880fe3c3931
pikca-c <vincimchan@gmail.com>
2024-06-16 22:25:04 +0100

    update about me

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,24 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about-section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div
+        className="hero--section--content--box
+        about--section"
+      >
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills--section--heading">About Me</h1>
+          <p className="hero--section-desciption">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <p className="hero--section-desciption">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,5 +1,6 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       {/*fragments*/}
       <HeroSection />
       <MySkills />
+      <AboutMe />
     </>
   );
 }
