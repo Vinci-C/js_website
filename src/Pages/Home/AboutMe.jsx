dd9a96475aa8095f4d564bd31cdf44650489babf
pikca-c <vincimchan@gmail.com>
2024-06-18 21:54:44 +0100

    update app.css

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -4,17 +4,14 @@ export default function AboutMe() {
       <div className="about--section--img">
         <img src="./img/about-me.png" alt="About Me" />
       </div>
-      <div
-        className="hero--section--content--box
-        about--section"
-      >
+      <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
           <p className="section--title">About</p>
           <h1 className="skills--section--heading">About Me</h1>
-          <p className="hero--section-desciption">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, magnam?
           </p>
-          <p className="hero--section-desciption">
+          <p className="hero--section-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
         </div>
